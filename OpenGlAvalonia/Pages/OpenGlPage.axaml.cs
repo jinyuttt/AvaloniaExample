5d7b5581b667e923a89dba782830aacb5c4d00b2
jinyuttt <jinyu20072787@sina.com>
2024-03-01 00:16:17 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

---
+++ OpenGlAvalonia/Pages/OpenGlPage.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OpenGlAvalonia.Pages
+{
+    public partial class OpenGlPage : UserControl
+    {
+        public OpenGlPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OpenGlAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OpenGlAvalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
