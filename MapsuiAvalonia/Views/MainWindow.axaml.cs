b1c3283511e7a7eb44f8e548cffc62177f489855
jinyucd <jinyucd@noreply.gitcode.com>
2024-12-18 23:02:01 +0800

    离线

--- MapsuiAvalonia/Views/MainWindow.axaml.cs
+++ MapsuiAvalonia/Views/MainWindow.axaml.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using BruTile;
 using Mapsui.Providers.Wms;
 using Mapsui.Widgets.ScaleBar;
+using System;
+using BruTile.MbTiles;
+using SQLite;
 
 namespace MapsuiAvalonia.Views
 {
@@ -30,19 +33,21 @@ namespace MapsuiAvalonia.Views
 
             var osmAttribution = new Attribution("© OpenStreetMap contributors", "https://www.openstreetmap.org/copyright");
             //  var osmSource = new HttpClientTileSource(httpClient, new GlobalSphericalMercator(), "http://online{s}.map.bdimg.com/onlinelabel/?qt=tile&x={x}&y={y}&z={z}&styles=pl&udt=20141103&scaler=1", new[] { "a", "b", "c" }, name: "OpenStreetMap", attribution: osmAttribution);
-            string url = "https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}";
-           // string url = "http://online3.map.bdimg.com/onlinelabel/?qt=tile&x={0}&y={1}&z={2}&styles=pl&udt=20200727&scaler=1&p=1";
-            var osmSource = new HttpClientTileSource(httpClient, new GlobalSphericalMercator(), url);
-            var osmLayer = new TileLayer(osmSource) { Name = "百度地图" };
-         
-            
-           // Map.Map.Widgets.Enqueue(new ScaleBarWidget(Map.Map) { TextAlignment = Mapsui.Widgets.Alignment.Center, HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Center, VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Top });
-        //   Map.Map.Widgets.Enqueue(new Mapsui.Widgets.Zoom.ZoomInOutWidget { MarginX = 20, MarginY = 40 });
+          //  string url = "https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}";
+            string url = "http://online3.map.bdimg.com/onlinelabel/?qt=tile&x={0}&y={1}&z={2}&styles=pl&udt=20200727&scaler=1&p=1";
+           // var osmSource = new HttpClientTileSource(httpClient, new GlobalSphericalMercator(), url);
+           // var osmLayer = new TileLayer(osmSource) { Name = "百度地图" };
 
-         
+            var mbtilesTilesource = new MbTilesTileSource(new SQLiteConnectionString("Resources/china.mbtiles", false));
+          //  var mbTilesTile =  mbtilesTilesource.GetTileAsync(new TileInfo { Index = new TileIndex(0, 0, 0) });
+            var osmLayer = new TileLayer(mbtilesTilesource) { Name = "百度地图" };
+            // Map.Map.Widgets.Enqueue(new ScaleBarWidget(Map.Map) { TextAlignment = Mapsui.Widgets.Alignment.Center, HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Center, VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Top });
+            //   Map.Map.Widgets.Enqueue(new Mapsui.Widgets.Zoom.ZoomInOutWidget { MarginX = 20, MarginY = 40 });
+
+
             Map.Map.Layers.Add(osmLayer);
-
            
+
         }
         private static WmsProvider CreateWmsProvider()
         {
